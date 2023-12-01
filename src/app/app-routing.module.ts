7bbf540c4fdd70e002529cb072f513714b6dcd3a
Gerard Paul Labitad <gerardl@surefirelocal.com>
2023-12-02 00:42:51 +0800

    Add routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,57 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminLayoutComponent } from './shared/layouts/admin/admin-layout/admin-layout.component';
+import { MainLayoutComponent } from './shared/layouts/main/main-layout/main-layout.component';
+import { DashboardComponent } from './views/admin/dashboard/dashboard.component';
+import { LoginComponent } from './views/components/login/login.component';
+import { AboutComponent } from './views/main/about/about.component';
+import { ContactComponent } from './views/main/contact/contact.component';
+import { HomeComponent } from './views/main/home/home.component';
+import { ProjectsComponent } from './views/main/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: '', component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'projects',
+        component: ProjectsComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      },
+      {
+        path: 'contact',
+        component: ContactComponent
+      },
+    ]
+  },
+  {
+    path: 'administrator', component: AdminLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
